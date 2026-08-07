fa6dabe8a622711179df936e35deb48a0d04d21c
david <davidakabueze067@gmail.com>
2026-08-07 15:43:12 +0100

    retouched the nav

--- src/nav_component/Navbar.jsx
+++ src/nav_component/Navbar.jsx
@@ -16,44 +16,44 @@ function Navbar() {
   };
 
   return (
-    <nav className="fixed bottom-0 flex items-center justify-center w-full lg:gap-20 gap-5 bg-white border-t py-5 text-xl z-50">
+    <nav className="fixed bottom-0 flex items-center justify-center w-full lg:gap-20 gap-5 bg-white border-t py-5 text-xl z-50 h-17">
       <NavLink to="/">
-        <div className="flex md:flex-row flex-col items-center justify-center lg:gap-2">
+        <div className="flex md:flex-row flex-col items-center justify-center md:gap-2 hover:bg-gray-200 hover:p-1 hover:px-5 rounded-xl transition-all duration-300">
           <FiHome />
           <p className="text-sm md:text-base">Home</p>
         </div>
       </NavLink>
 
       <NavLink to="/products">
-        <div className="flex md:flex-row flex-col items-center justify-center lg:gap-2">
+        <div className="flex md:flex-row flex-col items-center justify-center md:gap-2 hover:bg-gray-200 hover:p-1 hover:px-5 rounded-xl transition-all duration-300">
           <IoBagHandleOutline />
           <p className="text-sm md:text-base">Product</p>
         </div>
       </NavLink>
 
       <NavLink to="/cart">
-        <div className="flex md:flex-row flex-col items-center justify-center lg:gap-2">
+        <div className="flex md:flex-row flex-col items-center justify-center md:gap-2 hover:bg-gray-200 hover:p-1 hover:px-5 rounded-xl transition-all duration-300">
           <MdOutlineShoppingCart />
           <p className="text-sm md:text-base">Cart</p>
         </div>
       </NavLink>
 
       <NavLink to="/about">
-        <div className="flex md:flex-row flex-col items-center justify-center lg:gap-2">
+        <div className="flex md:flex-row flex-col items-center justify-center md:gap-2 hover:bg-gray-200 hover:p-1 hover:px-5 rounded-xl transition-all duration-300">
           <FaBorderAll />
           <p className="text-sm md:text-base">About</p>
         </div>
       </NavLink>
 
       <NavLink to="/settings">
-        <div className="flex md:flex-row flex-col items-center justify-center lg:gap-2">
+        <div className="flex md:flex-row flex-col items-center justify-center md:gap-2 hover:bg-gray-200 hover:p-1 hover:px-5 rounded-xl transition-all duration-300">
           <MdOutlineSettings />
           <p className="text-sm md:text-base">Setting</p>
         </div>
       </NavLink>
 
       {/* Dark/Light Mode Toggle Section */}
-      <div className="flex md:flex-row flex-col items-center justify-center">
+      <div className="flex md:flex-row flex-col items-center justify-center hover:bg-gray-200 hover:p-1 hover:px-5 rounded-xl transition-all duration-300">
         <label className=" relative inline-block w-12 h-6 cursor-pointer">
           <input
             type="checkbox"
@@ -61,9 +61,9 @@ function Navbar() {
             onChange={handleModeToggle}
             className="sr-only peer"
           />
-          <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-700"></div>
+          <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-700"></div>
         </label>
-        <p className="text-sm md:text-base font-medium">
+        <p className="text-sm md:text-base font-medium md:pl-2">
           {isDarkMode ? "Dark" : "Light"}
         </p>
       </div>
